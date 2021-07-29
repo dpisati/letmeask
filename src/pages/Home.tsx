7b64cc8bb41097a5941eedcb04cad17839e94b3b
dpisati <43629737+dpisati@users.noreply.github.com>
2021-07-29 21:05:24 +1200

    44 min aula 3

---
+++ src/pages/Home.tsx
@@ -0,0 +1,82 @@
+import { useHistory } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth';
+import { Button } from '../components/Button';
+
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg'
+import illustrationImg from '../assets/images/illustration.svg';
+
+import '../styles/auth.scss'
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
+
+
+export function Home() {
+    const history = useHistory();
+
+    const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode ] = useState("")
+
+    async function handleCreateRoom() {
+        if(!user) {
+            await signInWithGoogle()
+        }
+
+        history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === "") {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert("Room doesn't exist!")
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Illustration of questions and awnsers" />
+                <strong>Create rooms of Q&amp;A realtime experience.</strong>
+                <p>Answer your audience questions in real-time</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <button className="create-room" onClick={handleCreateRoom}>
+                        <img src={googleIconImg} alt="Google logo" />
+                        Create a room with Goole
+                    </button>
+
+                    <div className="separator">
+                        or enter ina room
+                    </div>
+
+                    <form onSubmit={handleJoinRoom}>
+                        <input
+                            type="text"
+                            placeholder="Type the room's name"
+                            value={roomCode}
+                            onChange={(e: any) => setRoomCode(e.target.value)}
+                        />
+                        <Button type="submit">
+                            Enter in the room
+                        </Button>
+                    </form>
+
+                </div>
+            </main>
+
+        </div>
+    )
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+import logoImg from '../assets/images/logo.svg';
+import illustrationImg from '../assets/images/illustration.svg';
+
+import '../styles/auth.scss'
+import { database } from '../services/firebase';
+
+export function NewRoom() {
+    const { user } = useAuth();
+    const history = useHistory();
+
+    const [newRoom, setNewRom] = useState("");
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+        
+        if(newRoom.trim() === "") {
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
+
+
+
+
+
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Illustration of questions and awnsers" />
+                <strong>Create rooms of Q&amp;A realtime experience.</strong>
+                <p>Answer your audience questions in real-time</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
+                    <h2>Create new room</h2>
+                    <form onSubmit={handleCreateRoom}>
+                        <input
+                            type="text"
+                            placeholder="Room name"
+                            value={newRoom}
+                            onChange={(event: any) => setNewRom(event.target.value)}
+                        />
+                        <Button type="submit">
+                            Create new room
+                        </Button>
+                    </form>
+
+                    <p>
+                        Do you wat to enter in a existing room?<Link to="/">Click here</Link>
+                    </p>
+
+
+                </div>
+            </main>
+
+        </div>
+    )
+}
